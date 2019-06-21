21e94410e9aa20262ee7d4a6f10ac9b70f6238d6
Ilton <xlsonplay@gmail.com>
2019-06-21 17:57:15 -0300

    Criando operacao de instanciacao

--- src/main/java/com/ilton/cursomc/CursomcApplication.java
+++ src/main/java/com/ilton/cursomc/CursomcApplication.java
@@ -7,14 +7,42 @@ Referência ao enunciado/origem do exercício: https://www.udemy.com/spring-boot
 
 package com.ilton.cursomc;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.ilton.cursomc.domain.Categoria;
+import com.ilton.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	
+	//Para fazer manipulação no banco de dados
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Colocar aqui a instanciação dos objetos
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		Categoria cat3 = new Categoria(null,"Industria");
+		Categoria cat4 = new Categoria(null,"Hospital");
+		
+		/*
+		Chamar uma operação para salvar no banco de dados
+		Savar uma lista no banco de dados
+		*/
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4));		
 	}
 
 }
