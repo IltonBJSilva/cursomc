85d0a0e758af3c94daee6cfa6453c7839976396f
Ilton <xlsonplay@gmail.com>
2019-11-21 14:40:04 -0300

    Instancia mais classes e relaciona elas.

--- src/main/java/com/ilton/cursomc/CursomcApplication.java
+++ src/main/java/com/ilton/cursomc/CursomcApplication.java
@@ -15,7 +15,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ilton.cursomc.domain.Categoria;
+import com.ilton.cursomc.domain.Cidade;
+import com.ilton.cursomc.domain.Cliente;
+import com.ilton.cursomc.domain.Endereco;
+import com.ilton.cursomc.domain.Estado;
 import com.ilton.cursomc.domain.Produto;
+import com.ilton.cursomc.domain.enums.TipoCliente;
 import com.ilton.cursomc.repositories.CategoriaRepository;
 import com.ilton.cursomc.repositories.ProdutoRepository;
 
@@ -63,6 +68,29 @@ public class CursomcApplication implements CommandLineRunner{
 		*/
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2));
+		
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+		Estado estado3 = new Estado(null, "Goias");
+
+		
+		Cidade cidade1 = new Cidade(null, "Uberlandia", estado1);
+		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
+		Cidade cidade3 = new Cidade(null, "Sante Helena de Goais", estado3);
+		Cidade cidade4 = new Cidade(null, "Goiania", estado3);
+		
+		Cliente cli1 = new Cliente(null,"Maria Silva","mari@gmail.com","3265409876", TipoCliente.PESSOAFISICA);
+		//Adicion ando telefone para o cliente
+		cli1.getTelefones().addAll(Arrays.asList("32324013","996708213"));
+		
+		/*Integer id, String logradouro, String numero, String complemento, 
+		String bairro, String cep,
+		Cliente cliente, Cidade cidade*/
+		Endereco e1 = new Endereco(null,"Rua Jose alves Garcias","1612","Apto-02","Brasil","38400668", cli1,cidade1);
+		Endereco e2 = new Endereco(null,"Rua Clarimundo carneiro","1356","Apto-07","Aparecida","38400668", cli1,cidade2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
 	}
 
 }
