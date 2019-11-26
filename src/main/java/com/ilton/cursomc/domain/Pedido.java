83c9da78650462891e8ba68bff43c2911a9905b3
Ilton <xlsonplay@gmail.com>
2019-11-26 18:04:03 -0300

    ItemPedido adicionado

--- src/main/java/com/ilton/cursomc/domain/Pedido.java
+++ src/main/java/com/ilton/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.ilton.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -11,8 +13,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Pedido implements Serializable{
@@ -34,6 +34,8 @@ public class Pedido implements Serializable{
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	@JoinColumn(name="pagamento_id")
 	private Pagamento pagamento;
+	
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 		
@@ -85,6 +87,13 @@ public class Pedido implements Serializable{
 
 	public void setPagamento(Pagamento pagamento) {
 		this.pagamento = pagamento;
+	}
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
