48a5ea6dfb92d4b5c872d177f9b55d57714bb242
Ilton <xlsonplay@gmail.com>
2019-12-17 14:27:23 -0300

    Inserindo uma nova categoria

--- src/main/java/com/ilton/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ilton/cursomc/resources/CategoriaResource.java
@@ -7,12 +7,16 @@ Referência ao enunciado/origem do exercício: https://www.udemy.com/spring-boot
 
 package com.ilton.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ilton.cursomc.domain.Categoria;
 import com.ilton.cursomc.services.CategoriaService;
@@ -24,7 +28,7 @@ import javassist.tools.rmi.ObjectNotFoundException;
 public class CategoriaResource {
 
 	// Acessando o serviço
-	@Autowired
+	@Autowired 
 	private CategoriaService service;
 
 	// Vai buscar por categoria/id no caso vai por id mostrando apenas as
@@ -33,5 +37,19 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	//@RequestBody faz o json ser convertido para java automaticamente
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		
+		obj = service.insert(obj);
+		//Ele pega essa URL localhost:8080/categorias e adiciona mais coisas na frente o ID
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		//Ja gera o 201 que informa que foi criado passando como paramentro o uri
+		return ResponseEntity.created(uri).build();
+		
 	}
 }
