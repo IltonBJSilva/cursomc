42dd69b6a1a488b1eab295a3b80c8500cb2ebf4f
Ilton <xlsonplay@gmail.com>
2019-12-18 13:51:47 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/ilton/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ilton/cursomc/resources/CategoriaResource.java
@@ -12,11 +12,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -65,7 +67,7 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public ResponseEntity<Categoria> delete(@PathVariable Integer id) throws ObjectNotFoundException {
+	public ResponseEntity<Void> delete(@PathVariable Integer id) throws ObjectNotFoundException{
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
@@ -76,5 +78,27 @@ public class CategoriaResource {
 		//
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			/*
+			  Essa anotação impede de inves de /categorias/page/0/20/ e assim por diante
+			  e inves disso, o melhor e assim:			  
+			  /categorias/page?page0&linesPerPage=20
+			   *
+			   * localhost:8080//categorias/page?linesPerPage=3&page=1&direction=DESC
+			*/
+			@RequestParam(value="page", defaultValue = "0") Integer page, 
+			//O defaultValue = 24 e melhor para vai encaixar direito 
+			@RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			//O defaultValue e para ordernar pelo oque, por exemplo eu quero odernar por nome
+			@RequestParam(value="orderBy", defaultValue = "nome") String orderBy, 
+			//O defaultValue dessa ja e para saber em qual direção se e ASC(crescente) ou DESC(decresente)
+			@RequestParam(value="direction", defaultValue = "ASC") String direction){
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		//
+		Page<CategoriaDTO> listDto  = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDto );
 	}
 }
